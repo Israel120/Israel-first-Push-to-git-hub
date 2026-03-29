7238966b3d6829668f8ff5f554979e2468d6c5f7
Abakpa Friday <abakpafriday1@gmailcom>
2026-03-29 21:30:42 +0100

    fix collapsible toggle

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,8 @@
-const collapsibles = document.querySelectorAll(".collapsible")
-collapsibles.forEach(function(item){
-    item.addEventListener("click",function(){
-        item.classList.toggle("collapsible__expanded")
-    })
-})
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach(item => {
+  const header = item.querySelector(".collapsible__header");
+  header.addEventListener("click", () => {
+    item.classList.toggle("collapsible__expanded");
+  });
+});
